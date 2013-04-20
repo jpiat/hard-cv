60f537e3b0e94a29c674dfb86646b022e56c900f
Jonathan Piat <jpiat@yaya.laas.fr>
2013-04-20 15:44:59 +0200

    new folder to host classifier, contains lut based color classifier

---
+++ hw/rtl/image/classifier/classifier_pack.vhd
@@ -0,0 +1,60 @@
+--
+--	Package File Template
+--
+--	Purpose: This package defines supplemental types, subtypes, 
+--		 constants, and functions 
+--
+--   To use any of the example code shown below, uncomment the lines and modify as necessary
+--
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+
+package classifier_pack is
+
+component color_classifier is
+port( clk	:	in std_logic ;
+		resetn	:	in std_logic ;
+		pixel_clock, hsync, vsync : in std_logic; 
+		pixel_clock_out, hsync_out, vsync_out : out std_logic; 
+ 		pixel_y : in std_logic_vector(7 downto 0) ;
+		pixel_u : in std_logic_vector(7 downto 0) ;
+		pixel_v : in std_logic_vector(7 downto 0) ;
+		pixel_class : out std_logic_vector(7 downto 0);
+		
+		--color lut interface 
+		color_index : out std_logic_vector(11 downto 0);
+		lut_in : in std_logic_vector(7 downto 0)
+);
+end component;
+end classifier_pack;
+
+package body classifier_pack is
+
+---- Example 1
+--  function <function_name>  (signal <signal_name> : in <type_declaration>  ) return <type_declaration> is
+--    variable <variable_name>     : <type_declaration>;
+--  begin
+--    <variable_name> := <signal_name> xor <signal_name>;
+--    return <variable_name>; 
+--  end <function_name>;
+
+---- Example 2
+--  function <function_name>  (signal <signal_name> : in <type_declaration>;
+--                         signal <signal_name>   : in <type_declaration>  ) return <type_declaration> is
+--  begin
+--    if (<signal_name> = '1') then
+--      return <signal_name>;
+--    else
+--      return 'Z';
+--    end if;
+--  end <function_name>;
+
+---- Procedure Example
+--  procedure <procedure_name>  (<type_declaration> <constant_name>  : in <type_declaration>) is
+--    
+--  begin
+--    
+--  end <procedure_name>;
+ 
+end classifier_pack;
---
+++ hw/rtl/image/classifier/color_classifier.vhd
@@ -0,0 +1,70 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    14:58:56 04/20/2013 
+-- Design Name: 
+-- Module Name:    color_classifier - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity color_classifier is
+port( clk	:	in std_logic ;
+		resetn	:	in std_logic ;
+		pixel_clock, hsync, vsync : in std_logic; 
+		pixel_clock_out, hsync_out, vsync_out : out std_logic; 
+ 		pixel_y : in std_logic_vector(7 downto 0) ;
+		pixel_u : in std_logic_vector(7 downto 0) ;
+		pixel_v : in std_logic_vector(7 downto 0) ;
+		pixel_class : out std_logic_vector(7 downto 0);
+		
+		--color lut interface 
+		color_index : out std_logic_vector(11 downto 0);
+		lut_in : in std_logic_vector(7 downto 0)
+);
+end color_classifier;
+
+architecture Behavioral of color_classifier is
+
+begin
+
+color_index <= pixel_y(7 downto 4) & pixel_u(7 downto 4) & pixel_v(7 downto 4) ;
+
+process(clk, resetn)
+begin
+	if resetn = '0' then
+		pixel_clock_out <= '0';
+		hsync_out <= '0' ;
+		vsync_out <= '0' ;
+	elsif clk'event and clk = '1' then
+		pixel_clock_out <= pixel_clock;
+		hsync_out <= hsync ;
+		vsync_out <= vsync ;
+	end if ;
+end process ;
+
+pixel_class <= lut_in ;
+
+end Behavioral;
+
