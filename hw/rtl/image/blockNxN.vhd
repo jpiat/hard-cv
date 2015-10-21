a37e6cf830dd57852bd18045a845d41a400374e0
Jonathan Piat <piat.jonathan@gmail.com>
2015-10-21 09:04:04 +0200

    Updated blockNxN description to use less external components.

--- hw/rtl/image/blockNxN.vhd
+++ hw/rtl/image/blockNxN.vhd
@@ -24,7 +24,6 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 library WORK ;
 use WORK.image_pack.ALL ;
-use WORK.primitive_pack.ALL ;
 use WORK.utils_pack.ALL ;
 
 -- Uncomment the following library declaration if using
@@ -59,21 +58,16 @@ type std_vecN is array (0 to (N- 1)) of std_logic_vector(8 downto 0);
 signal blockNxN : matNM(0 to N-1, 0 to N-1) ;
 
 signal std_blockNxN	: std_matN ;					
-
-
-
 signal LINEI_INPUT, LINEI_OUTPUT : std_vecN ;
 signal lpixel_data : std_logic_vector(8 downto 0);
 
 signal INPUT_LINES, OUTPUT_LINES : std_logic_vector((((N - 1)*8) - 1) downto 0) ;
-signal enable_linen_latches : std_logic_vector(N-1 downto  0); 
-
 signal enable_lines_latches : std_logic ;
 
 signal nb_line : std_logic_vector((nbit(HEIGHT) - 1) downto 0) := (others => '0');
 signal pixel_counterq, pixel_counterq_delayed : std_logic_vector((nbit(WIDTH) - 1) downto 0) := (others => '0');
 
-signal old_pixel_in_clk, pixel_in_clk_rising_edge, new_blockq : std_logic ;
+signal old_pixel_in_clk, pixel_in_clk_rising_edge : std_logic ;
 
 begin
 
@@ -83,7 +77,7 @@ linesn: dpram_NxN
 	generic map(SIZE => WIDTH + 1 , NBIT => (((N - 1)*8)), ADDR_WIDTH => nbit(WIDTH))
 	port map(
  		clk => clk, 
- 		we => new_blockq ,
+ 		we => enable_lines_latches ,
 		dpo => OUTPUT_LINES,
 		dpra => pixel_counterq,
  		di => INPUT_LINES,
@@ -102,15 +96,6 @@ end generate gen_mem_input_0;
  
 lpixel_data <= ( '0' & pixel_in_data) ;
 
-enable_lines_latches <= (NOT pixel_in_hsync and pixel_in_clk) ;
-
-
-gen_line_latches : for I in 0 to (N - 1) generate
-	enable_linen_latches(I) <= enable_lines_latches when nb_line > I else
-								'0' ;
-end generate gen_line_latches;
-
-
 process(clk, resetn)
 begin
 	if resetn = '0' then
@@ -120,10 +105,8 @@ begin
 	end if ;
 end process ;
 pixel_in_clk_rising_edge <= ((NOT old_pixel_in_clk) AND pixel_in_clk) ;
-new_blockq <= pixel_in_clk_rising_edge when pixel_in_hsync = '0' else
-				 '0' ;
-new_block <= new_blockq ;
-
+enable_lines_latches <= (NOT pixel_in_hsync and pixel_in_clk_rising_edge) ;
+new_block <= enable_lines_latches ;
 
 convert_cols_std : for C in 0 to (N-1) generate
 	convert_rows_std : for L in 0 to (N-1) generate
@@ -134,47 +117,49 @@ end generate convert_cols_std;
 
 gen_latches_row : for I in 0 to (N-1) generate
 	gen_latches_col : for J in 0 to (N-1) generate
-		
-		left_cols : if j < N-1 generate
-			latch_i_i: edge_triggered_latch
-						  generic map(NBIT => 9)
-						  port map(
-							clk => clk ,
-							resetn => resetn ,
-							sraz =>pixel_in_vsync ,
-							en => enable_lines_latches,
-							d => std_blockNxN(I, J+1), 
-							q => std_blockNxN(I, J)
-						  );
-		end generate left_cols;
-		right_cols : if j = (N - 1) and i /= (N - 1) generate
-			latch_0_2: edge_triggered_latch
-						  generic map(NBIT => 9)
-						  port map(
-							clk => clk ,
-							resetn => resetn ,
-							sraz =>pixel_in_vsync ,
-							en => enable_lines_latches,
-							d => LINEI_OUTPUT(I), 
-							q => std_blockNxN(I, (N - 1))
-						  );
-		end generate right_cols;
-		
-		right_col_n : if i = (N - 1) and j = (N - 1) generate
-			latch_i_i: edge_triggered_latch
-						  generic map(NBIT => 9)
-						  port map(
-							clk => clk ,
-							resetn =>resetn ,
-							sraz =>pixel_in_vsync,
-							en => enable_lines_latches,
-							d => lpixel_data, 
-							q => std_blockNxN(I,I)
-						  );
-		end generate right_col_n;
+	
+	-- shift columns in the block
+	shift_cols : if J < N-1 generate
+		process(clk, resetn) 
+		begin
+			if resetn = '0' then
+				std_blockNxN(I, J) <= (others => '0');
+			elsif rising_edge(clk) then
+				if enable_lines_latches = '1' then
+					std_blockNxN(I, J) <= std_blockNxN(I, J+1);
+				end if ;
+			end if ;
+		end process ;
+	end generate ;
+	
+	-- fetch data from memory for rightmost column
+	in_col : if J = N-1 and I /= N-1 generate
+		process(clk, resetn) 
+		begin
+			if resetn = '0' then
+				std_blockNxN(I, J) <= (others => '0');
+			elsif rising_edge(clk) then
+				if enable_lines_latches = '1' then
+					std_blockNxN(I, J) <= LINEI_OUTPUT(I);
+				end if ;
+			end if ;
+		end process ;
+	end generate ;
+	
 	end generate gen_latches_col; 
 end generate gen_latches_row; 
 
+--Latch incoming pixel
+process(clk, resetn) 
+begin
+	if resetn = '0' then
+		std_blockNxN(N-1, N-1) <= (others => '0');
+	elsif rising_edge(clk) then
+		if enable_lines_latches = '1' then
+			std_blockNxN(N-1, N-1) <= lpixel_data;
+		end if ;
+	end if ;
+end process ;
 
 pixel_counter0: pixel_counter
 		generic map(MAX => WIDTH)
@@ -185,55 +170,35 @@ pixel_counter0: pixel_counter
 			pixel_count => pixel_counterq
 			);
 			
-delay_counter: edge_triggered_latch
-		generic map(NBIT => nbit(WIDTH))
-		port map(
-			clk => clk ,
-			resetn =>resetn ,
-			sraz =>pixel_in_hsync,
-			en => enable_lines_latches,
-			d => pixel_counterq, 
-			q => pixel_counterq_delayed
-			);
+--Delay pixel count to generate write address to memory
+process(clk, resetn) 
+begin
+	if resetn = '0' then
+		pixel_counterq_delayed <= (others => '0');
+	elsif rising_edge(clk) then
+		if enable_lines_latches = '1' then
+			pixel_counterq_delayed <= pixel_counterq;
+		end if ;
+	end if ;
+end process ;			
 			
+-- used to generate 0 pixels in block for the N-1 first lines
 line_counter0: line_counter
 		generic map(MAX => HEIGHT)
 		port map(
 			clk => clk,
 			resetn => resetn, 
-			hsync =>pixel_in_hsync,pixel_in_vsync =>pixel_in_vsync, 
+			pixel_in_hsync =>pixel_in_hsync,
+			pixel_in_vsync => pixel_in_vsync,
 			line_count => nb_line
 			);
-			
 	
-	
+-- This make sure that on the image edge, the uninitialized pixels of the block are filled with zero	
 zero_cols_std : for C in 0 to (N-1) generate
 	zero_rows_std : for L in 0 to (N-1) generate
 		block_out(L,C) <= blockNxN(L,C) when pixel_counterq > ((N-1) - C) and nb_line > ((N-2) - L) else
 						 (others => '0');
 	end generate zero_rows_std; 
 end generate zero_cols_std; 	
-	
-
---block_out(0)(0) <= block3x3(0)(0) when pixel_counterq > 2 and nb_line > 1 else
---						 (others => '0');
---block_out(0)(1) <= block3x3(0)(1) when pixel_counterq > 1 and nb_line > 1 else
---						(others => '0');
---block_out(0)(2) <= block3x3(0)(2) when nb_line > 1 else 
---						(others => '0');
---						
---block_out(1)(0) <= block3x3(1)(0) when pixel_counterq > 2 and nb_line > 0 else
---						 (others => '0');
---block_out(1)(1) <= block3x3(1)(1) when pixel_counterq > 1 and nb_line > 0 else
---						(others => '0');
---block_out(1)(2) <= block3x3(1)(2) when nb_line > 0 else 
---						(others => '0');
---
---block_out(2)(0) <= block3x3(2)(0) when pixel_counterq > 2 else
---						 (others => '0');
---block_out(2)(1) <= block3x3(2)(1) when pixel_counterq > 1  else
---						(others => '0');
---
---block_out(2)(2) <= block3x3(2)(2) ;
 
 end RTL;
--- hw/testbench/block3x3_tb.vhd
+++ hw/testbench/block3x3_tb.vhd
@@ -8,152 +8,72 @@
   LIBRARY work ;
   use work.image_pack.all ;
    use work.filter_pack.all ;
-	use work.utils_pack.all ;
+  ENTITY block_tb IS
+  END block_tb;
 
-  ENTITY testbench IS
-  END testbench;
+  ARCHITECTURE behavior OF block_tb IS 
 
-  ARCHITECTURE behavior OF testbench IS 
-
+	component virtual_camera is
+	generic(IMAGE_PATH : string ; PERIOD : time := 10ns);
+	port(
+		clk : in std_logic; 
+		resetn : in std_logic; 
+		pixel_data : out std_logic_vector(7 downto 0 ); 
+		pixel_out_clk, pixel_out_hsync, pixel_out_vsync : out std_logic );
+	end component;
   -- Component Declaration
 
          constant clk_period : time := 5 ns ;
 			constant pclk_period : time := 40 ns ;
 			
 			signal clk, resetn : std_logic ;
-			signal pxclk,pixel_in_hsync,pixel_in_vsync : std_logic ;
+			signal pixel_in_clk,pixel_in_hsync,pixel_in_vsync : std_logic ;
 			signal pxclk_out, pixel_out_hsync, pixel_out_vsync : std_logic ;
 			signal pxclk_out_synced, pixel_out_hsync_synced, pixel_out_vsync_synced : std_logic ;
 			signal new_block : std_logic ;
-			--signal block_out :  mat3 ;
-			signal block_out : matNM(0 to 2, 0 to 2) ;
-			signal pixel, pixel_out, pixel_out_synced: std_logic_vector(7 downto 0 ) := (others => '0');
+			signal block_out : matNM(0 to 4, 0 to 4) ;
+			signal pixel_in_data: std_logic_vector(7 downto 0 ) := (others => '0');
 
   BEGIN
-         block3X3v3_0 :  block3X3_pixel_pipeline_sp 
-				port map(
-						resetn => resetn, 
-						pixel_in_clk => pxclk, 
-						hsync =>pixel_in_hsync, 
-						vsync =>pixel_in_vsync,
-						pixel_in_data => pixel, 
-						pixel_out_clk => pxclk_out, 
-						pixel_out_hsync => pixel_out_hsync, 
-						pixel_out_vsync => pixel_out_vsync,
-						block_out => block_out
-				);
-				
---			sobel0 : sobel3x3_pixel_pipeline
---				port map(
---										resetn => resetn, 
---										pixel_in_clk => pxclk, 
---										hsync =>pixel_in_hsync, 
---										vsync =>pixel_in_vsync,
---										pixel_in_data => pixel, 
---										pixel_out_clk => pxclk_out, 
---										pixel_out_hsync => pixel_out_hsync, 
---										pixel_out_vsync => pixel_out_vsync,
---										pixel_out_data => pixel_out
---				);
---				hyst0 : hyst_threshold_pixel_pipeline
---				port map(
---										resetn => resetn, 
---										pixel_in_clk => pxclk, 
---										hsync =>pixel_in_hsync, 
---										vsync =>pixel_in_vsync,
---										pixel_in_data => pixel, 
---										pixel_out_clk => pxclk_out, 
---										pixel_out_hsync => pixel_out_hsync, 
---										pixel_out_vsync => pixel_out_vsync,
---										pixel_out_data => pixel_out
---				);
---				
---				
---			bridge0: clock_bridge 
---				generic map(SIZE => 10)
---				port map(
---						clk_fast => clk, clk_slow => pxclk_out, resetn => resetn ,
---						clk_slow_out => pxclk_out_synced ,
---						data_in(0) =>pixel_out_hsync,
---						data_in(1) =>pixel_out_vsync,
---						data_in(9 downto 2) =>pixel_out,
---						data_out(0) =>pixel_out_hsync_synced,
---						data_out(1) =>pixel_out_vsync_synced,
---						data_out(9 downto 2) =>pixel_out_synced
---						);
-								
---			  block3X3v3_0 :  block3X3
---				port map(
---						clk => clk ,
---						resetn => resetn, 
---						pixel_in_clk => pxclk, 
---						hsync =>pixel_in_hsync, 
---						vsync =>pixel_in_vsync,
---						pixel_in_data => pixel, 
---						new_block => new_block ,
---						block_out => block_out
---				);
-				
---				block3X3v3_0 :  blockNxN
---				generic map (N => 5)
---				port map(
---						clk => clk ,
---						resetn => resetn, 
---						pixel_in_clk => pxclk, 
---						hsync =>pixel_in_hsync, 
---						vsync =>pixel_in_vsync,
---						pixel_in_data => pixel, 
---						new_block => new_block ,
---						block_out => block_out
---				);
-				
 
-	process
+main_clk : process
+	begin
+		clk <= '1' ;
+		wait for clk_period/2 ;
+		clk <= '0';
+		wait for clk_period/2 ;
+	end process ;
+	
+	main_reset : process
 	begin
 		resetn <= '0' ;
-		wait for 10*clk_period;
+		wait for pclk_period * 100;
 		resetn <= '1' ;
-		while true loop
-			clk <= '0';
-			wait for clk_period;
-			clk <= '1';
-			wait for clk_period; 
-		end loop ;
-	end process;
-	
-process
-	variable px_count, line_count, byte_count : integer := 0 ;
-	begin
-		pxclk <= '0';
-		if px_count < 639 and line_count >= 20 and line_count < 497 then
-			hsync <= '0' ;
-			pixel <= pixel + 1;
-		else
-				hsync <= '1' ;
-		end if ;
-
-		if line_count < 3 then
-			vsync <= '1' ;
-		 else 
-			vsync <= '0' ;
-		end if ;
-		wait for pclk_period;
-		
-		pxclk <= '1';
-		if (px_count = 784 ) then
-			px_count := 0 ;
-			if (line_count > 510) then
-			   line_count := 0;
-		  else
-		    line_count := line_count + 1 ;
-		  end if ;
-		else
-		  px_count := px_count + 1 ;
-		end if ;
-		
-		wait for pclk_period;
-
-	end process;
+		wait ;
+	end process ;
+ 
+	cam0 : virtual_camera
+	generic map(IMAGE_PATH => "/home/jpiat/Pictures/3b3_square.pgm", PERIOD => pclk_period)
+	port map(
+		clk => clk,
+		resetn => resetn,
+		pixel_data =>  pixel_in_data,
+		pixel_out_clk => pixel_in_clk,
+		pixel_out_hsync =>pixel_in_hsync ,
+		pixel_out_vsync =>pixel_in_vsync);
+				
+		blockNXN_U0 :  blockNxN
+		generic map (N => 5)
+		port map(
+				clk => clk ,
+				resetn => resetn, 
+				pixel_in_clk => pixel_in_clk, 
+				pixel_in_hsync =>pixel_in_hsync, 
+				pixel_in_vsync =>pixel_in_vsync,
+				pixel_in_data => pixel_in_data, 
+				new_block => new_block ,
+				block_out => block_out
+		);
   
 
   END;
